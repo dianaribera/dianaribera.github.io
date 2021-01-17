69d206e0b07e6c167cd1836626293176a6881fc9
Diana Ribera <dianaribera@byui.edu>
2021-01-16 20:55:58 -0700

    week 2 assignment

---
+++ week_2/js/myScript.js
@@ -0,0 +1,7 @@
+let d = new Date();
+let y = d.getFullYear();
+document.getElementById("currentYear").innerHTML = y;
+
+var x = document.lastModified;
+document.getElementById("dateLastModified").innerHTML = x;
+
