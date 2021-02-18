d93938cbe247895e9132856f5451d030e9e8ba52
dianaribera <dianaribera@byui.edu>
2021-02-18 16:02:04 -0700

    weather gallery

--- lesson7/js/scripts.js
+++ lesson7/js/scripts.js
@@ -1,3 +1,21 @@
+function toggleMenu() {
+  console.log(document.getElementById("primaryNav").classList);
+  document.getElementById("primaryNav").classList.toggle("hide");
+}
+
+var current = new Date();
+var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+document.getElementById("currentDate").innerHTML = day[current.getDay()] +
+  ", " + current.getDate() + " " + month[current.getMonth()] + ", " +
+  current.getFullYear();
+
+  
+if (current.getDay() == 5) {
+  document.getElementById("breakfast").style.display = "block";
+}
+
 let imagesToLoad = document.querySelectorAll('img[data-src]');
 const loadImages = (image) => {
   image.setAttribute('src', image.getAttribute('data-src'));
