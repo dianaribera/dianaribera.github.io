a4d6000c16d1eae3536d38e854689fa51d5663b2
dianaribera <dianaribera@byui.edu>
2021-02-26 11:32:09 -0700

    pancake javascript error

--- lesson8/js/scripts.js
+++ lesson8/js/scripts.js
@@ -11,10 +11,6 @@ document.getElementById("currentDate").innerHTML = day[current.getDay()] +
     ", " + current.getDate() + " " + month[current.getMonth()] + ", " +
     current.getFullYear();
 
-    
-if (current.getDay() == 5) {
-    document.getElementById("breakfast").style.display = "block";
-}
 
 function adjustRating(rating) {
     document.getElementById("ratingvalue").innerHTML = rating;
