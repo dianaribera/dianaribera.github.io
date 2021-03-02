5aa5ca5d2cf34a8bc51262a4dd57a391db9ebce7
dianaribera <dianaribera@byui.edu>
2021-03-02 15:31:13 -0700

    prophet cards

---
+++ lesson9/js/prophets.js
@@ -0,0 +1,28 @@
+const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    const prophets = jsonObject['prophets'];
+    //console.table(jsonObject);  // temporary checking for valid response and data parsing
+    for (let i = 0; i < prophets.length; i++ ) {
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+        let p = document.createElement('p');
+        let img = document.createElement('img');
+
+        h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+        card.appendChild(h2);
+
+        p.innerHTML = 'Date of Birth: ' + prophets[i].birthdate + '<br>' + 'Place of Birth: ' + prophets[i].birthplace;
+        card.appendChild(p);
+
+        img.setAttribute('src', prophets[i].imageurl);
+        card.appendChild(img);
+
+        document.querySelector('div.cards').appendChild(card);
+ 
+    }
+  });
