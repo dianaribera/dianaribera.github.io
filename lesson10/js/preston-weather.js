383fb367282bfe48deaaf3d85e1536ddcade3ffb
dianaribera <dianaribera@byui.edu>
2021-03-11 12:45:22 -0700

    lesson 10

---
+++ lesson10/js/preston-weather.js
@@ -0,0 +1,47 @@
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=3738fdc9983f5ba04c511ad6a0cc5685';
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    document.getElementById('current-cond').textContent = jsObject.weather[0].description;
+    document.getElementById('high-temp').textContent = Math.round(jsObject.main.temp_max) + "°F";
+    document.getElementById('humidity').textContent = jsObject.main.humidity + "%";
+    document.getElementById('windSpeed').textContent = jsObject.wind.speed +" mph";
+
+    const temp = jsObject.main.temp;
+    const speed = jsObject.wind.speed;
+    const result = windChill(temp, speed)
+
+    function windChill(t, s) {
+        let compute = 35.74 + (0.6215 * t) - 35.75 * Math.pow(s, 0.16) + (0.4275 * t * Math.pow(s, 0.16));
+        let total = Math.round(compute);
+        return total; 
+    }
+
+    if ((temp > 50) || (speed < 3)) {
+        document.getElementById("windChillOutput").innerHTML = "NA";
+    } else {
+        document.getElementById("windChillOutput").innerHTML = result + "&deg;F";
+    }
+  });
+
+  const forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=3738fdc9983f5ba04c511ad6a0cc5685';
+
+  fetch(forecastURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+
+    var forecast = jsObject.list.filter(x => x.dt_txt.includes('18:00:00'));
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    for (let day = 0; day < forecast.length; day++) {
+      const d = new Date(forecast[day].dt_txt);
+      const imagesrc = 'https://openweathermap.org/img/w/' + forecast[day].weather[0].icon + '.png';
+      const desc = forecast[day].weather[0].description;
+      document.getElementById(`day${day+1}`).textContent = days[d.getDay()];
+      document.getElementById(`temp${day+1}`).textContent = Math.round(forecast[day].main.temp) + '°F';
+      document.getElementById(`icon${day+1}`).setAttribute('src', imagesrc);
+      document.getElementById(`icon${day+1}`).setAttribute('alt', desc);
+    }
+
+  });
